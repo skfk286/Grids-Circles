5f3cfb81d11d75637daa9b3a37f2f6af008546d5
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-06 14:33:10 +0900

    [init]

---
+++ src/test/java/org/example/gc_coffee/GcCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gc_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
